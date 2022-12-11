82989b4f6b18f5effd220dcc27eb0f3836fece01
suc <1215618342@qq.com>
2022-12-11 22:57:58 +0800

    fix:优化

--- webrtc-link-demo/src/views/demo04-janus-01.vue
+++ webrtc-link-demo/src/views/demo04-janus-01.vue
@@ -1,19 +1,29 @@
 <template>
 	<div style="width: 98%;height: 95vh%;margin-top: 20px;">
-		<el-row :gutter="20">
-			<el-col :span="12" style="display: flex;flex-direction: row;">
-				<el-input placeholder="请输入用户名" v-model="username"></el-input>
-				<el-button type="success" @click="registerUser">注册</el-button>
-				<el-input placeholder="请输入被呼叫者用户名" v-model="targetUserName"></el-input>
-				<el-button type="primary" @click="call">呼叫</el-button>
-			</el-col>
+		<el-row :gutter="20" style="display: flex;flex-direction: row;justify-content: center;">
+			<el-input style="width:130px;" placeholder="请输入用户名" v-model="username"></el-input>
+			<el-button type="success" @click="registerUser">注册</el-button>
+			<el-input style="width:130px;" placeholder="请输入被呼叫者用户名" v-model="targetUserName"></el-input>
+			<el-button type="primary" @click="call">呼叫</el-button>
 		</el-row>
 		<el-row>
 			<div class="streams">
-				<video id="localDomId" height="300px" width="300px" controls muted></video>
-				<video id="remoteVideo" height="300px" width="300px" controls></video>
+				<video id="localDomId" style="object-fit: fill;" height="400px" width="50%" controls muted></video>
+				<video id="remoteVideo" style="object-fit: fill;" height="400px" width="50%" controls></video>
 			</div>
 		</el-row>
+		
+		<el-row style="position: fixed;bottom: 220px;width: 100%;">
+			<div>
+				<el-button type="danger" size="mini" round @click="hangup()">挂断</el-button>
+				<el-button type="warning" size="mini" round @click="record()">录制</el-button>
+				<el-button v-if="!videoStatus" type="warning" size="mini" round @click="controlVideo()">打开视频</el-button>
+				<el-button v-if="videoStatus" type="primary" size="mini" round @click="controlVideo()">关闭视频</el-button>
+				<el-button v-if="!audioStatus" type="warning" size="mini" round @click="controlAudio()">打开麦克风</el-button>
+				<el-button v-if="audioStatus" type="primary" size="mini" round @click="controlAudio()">关闭麦克风</el-button>
+			</div>
+		</el-row>
+		
 		
 	</div>
 </template>
@@ -97,18 +107,25 @@
 				},
 				username:undefined,//用户名
 				targetUserName:undefined,//被呼叫方
+				bitrateTimer:null,
+				videoStatus:true,//是否视频
+				audioStatus:true,//是否音频
 			}
 		},
 		created() {
 			initInnerLocalDevice()
 			this.localDevice = localDevice
 			this.initJanus()
+			if(this.bitrateTimer){
+				clearInterval(this.bitrateTimer)
+			}
+
 		},
 		methods:{
 			initJanus(){
 			  const that = this;
 			  Janus.init({
-				debug: true,
+				debug: false,
 				dependencies: Janus.useDefaultDependencies({
 				  adapter: adapter
 				}),
@@ -151,6 +168,10 @@
 				            //插件初始化成功后 pluginHandle 就是全局句柄，通过 pluginHandle可以操作当前
 				            //会话的所有功能
 							videoCallPluginHandle = pluginHandle
+							console.log("视频插件初始化成功")
+							// setInterval(() => {
+							// 	that.getBitrate()
+							// },1500)
 							// console.log("视频呼叫插件初始化成功",videoCallPluginHandle)
 				        },
 				        error: function(cause) {
@@ -166,7 +187,7 @@
 				        onlocaltrack: function(track, added) {
 				            // 本地媒体流发布后可以监听
 							console.log("本地媒体",track,added)
-							if(added){
+							if(added===true){
 								that.setDomVideoTrick("localDomId",track)
 							}
 							
@@ -174,7 +195,7 @@
 				        onremotetrack: function(track, mid, added) {
 				            // 远端媒体流
 							console.log("远程媒体",track,mid,added)
-							if(added){
+							if(added===true){
 								that.setDomVideoTrick("remoteVideo",track)
 							}
 				        },
@@ -206,7 +227,7 @@
 					//双向语音视频+datachannel
 					tracks: [
 						{ type: 'audio', capture: true, recv: true },
-						{ type: 'video', capture: true, recv: true },
+						{ type: 'video', capture: true, recv: true,simulcast: false },
 						{ type: 'data' },
 					],
 					success: function(jsep) {
@@ -220,6 +241,7 @@
 				});
 			},
 			onMessageForVideoCall(msg,jsep){
+				const that = this
 				console.log(" ::: Got a message :::", msg);
 				var result = msg["result"];
 				if(result) {
@@ -230,9 +252,11 @@
 						var event = result["event"];
 						if(event === 'registered') {
 							console.log("注册成功",msg)
+							that.messageNotify("注册成功")
 							videoCallPluginHandle.send({ message: { request: "list" }});
 						} else if(event === 'calling') {
 							console.log("呼叫中")
+							that.messageNotify("呼叫中，请稍后")
 						} else if(event === 'incomingcall') {
 							let username = result["username"]
 							console.log("来自于 【"+username+"】的呼叫")
@@ -254,7 +278,10 @@
 								});
 						} else if(event === 'accepted') {
 							console.log("对方已接听同时设置协商信息",jsep)
-							if(jsep){videoCallPluginHandle.handleRemoteJsep({ jsep: jsep });}
+							if(jsep){
+								videoCallPluginHandle.handleRemoteJsep({ jsep: jsep });
+							}
+							that.messageNotify("对方已接听")
 						} else if(event === 'update') {
 							// An 'update' event may be used to provide renegotiation attempts
 							if(jsep) {
@@ -282,14 +309,17 @@
 						} else if(event === 'hangup') {
 							console.log(result["username"] + "已挂断,原因:(" + result["reason"] + ")!");
 							videoCallPluginHandle.hangup();
+							that.messageNotify("已挂断")
+							that.clearMedia()
 						} else if(event === "simulcast") {
-							console.log("联播simulcast，暂时不用考虑")
+							console.log("联播simulcast，暂时不用考虑",msg)
 						}
 					}
 				} else {
 					// 出错
 					var error = msg["error"];
 					console.log("未知异常",msg)
+					that.messageNotify(error)
 					//挂断
 					videoCallPluginHandle.hangup();
 				}
@@ -342,7 +372,64 @@
 					// video.style.width = '100%'
 					// video.style.height = '100%'
 				}
-			}
+			},
+			
+			hangup(){
+				videoCallPluginHandle.hangup()
+				this.clearMedia()
+			},
+			clearMedia(){
+				let local = document.getElementById('localDomId')
+				if(local && local.srcObject){
+					local.srcObject.getTracks().forEach(e => {
+						e.stop()
+					})
+					local.srcObject = null
+				}
+				let remote = document.getElementById('remoteVideo')
+				if(remote && remote.srcObject){
+					remote.srcObject.getTracks().forEach(e => {
+						e.stop()
+					})
+					remote.srcObject = null
+				}
+				this.audioStatus = true
+				this.videoStatus = true
+			},
+			record(){
+				videoCallPluginHandle.send({ message:
+					{ request: "set", record: true,filename:'/home/janus-gateway/record/'+this.username+'-'+this.targetUserName, },
+				});
+			},
+			bitrateSet(){
+				videoCallPluginHandle.send({ message:
+					{ request: "set", bitrate: 400*1000 },
+				});
+			},
+			controlVideo(){
+				this.videoStatus = !this.videoStatus
+				videoCallPluginHandle.send({ message:
+					{ request: "set", video:  this.videoStatus},
+				});
+			},
+			controlAudio(){
+				this.audioStatus = !this.audioStatus
+				videoCallPluginHandle.send({ message:
+					{ request: "set", audio:  this.audioStatus},
+				});
+			},
+			getBitrate(){
+				if(videoCallPluginHandle){
+					console.log(videoCallPluginHandle.getBitrate())
+				}	
+			},
+			messageNotify(msg){
+				 this.$notify({
+					  title: '温馨提示',
+					  message: msg,
+					  type: 'warning'
+				});
+			},
 			
 
 		}
